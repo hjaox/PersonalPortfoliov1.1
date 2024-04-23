cbc042b7abaf8d19df985442d1ae78df55170b89
hjaox <hectorjana.o@gmail.com>
2024-04-23 14:36:03 +0100

    saved react portfolion to a different repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Welcome to my portfolio repository.
+
+## v1
+The first version of my portfolio. Made with vanilla HTML, CSS and Javascript. You can view the source code at the vanilla branch of this repository check out the site [here](https://hjaox.github.io/)
+
+## v1.1
+The latest version of my portfolio. Made with React and JS. You can view the source code at the react branch of this repository or check out the site [here](https://hectorobanana.netlify.app/)
---
+++ data/projects.js
@@ -0,0 +1,43 @@
+export const projects = {
+  "NC News Backend Project": {
+    info: "A RESTful API backend project that would serve data of articles and other related information to the frontend project that I built.\n\nThe purpose or this repository is to learn how to build an API for accessing application data programmatically and to mimic the building of a real-world backend service.",
+    tech: ["JavaScript", "PostgreSQL", "Express", "Jest", "Supertest", "node-Postgres"],
+    sourceCode: "https://github.com/hjaox/be-nc-news",
+    appLink: null,
+    preview: "./media/projects/ncNewsBackend-preview.png",
+    shortVid: "",
+  },
+  "NC News Frontend Project": {
+    info: "A frontend architecture for the backend that I have built using React.",
+    tech: ["React", "Axios", "CSS", "JavaScript"],
+    sourceCode: "https://github.com/hjaox/fe-nc-news",
+    appLink: "https://nc-news-hjaox.netlify.app/",
+    preview: "./media/projects/ncNewsFrontend-preview.png",
+    shortVid: "",
+  },
+  "My Personal Portfolio v1": {
+    info: "Earlier version of my portfolio made with vanilla HTML, CSS and JavaScript.",
+    tech: ["HTML", "CSS", "JavaScript"],
+    sourceCode: "https://github.com/hjaox/hjaox.github.io/tree/vanilla",
+    appLink: "https://hjaox.github.io/",
+    preview: "./media/projects/personalPortfoliov1-preview.png",
+    shortVid: "",
+  },
+  "Swim Wild": {
+    info: "A social platform for a trend of wild swimming. The app provides a blend of user-generated and web scrapped content to inform users about nearby swimming spots that features safety consideration. Users can share their swimming experience with others, use statistics about their swimming history and learn more about their favourite spots via our API network and analysis of user content.\n\nThis app was a group project consisting of 6 people during our time with Northcoders",
+    tech: ["React-Native", "Node.js", "MongoDB", "Firebase", "Jest", "Redux"],
+    sourceCode: null,
+    appLink: null,
+    presentation: "https://www.youtube.com/watch?v=cNxkq7k3X90",
+    preview: "./media/projects/swimWild-preview.png",
+    shortVid: "",
+  },
+  "My Personal Portfolio v1.1": {
+    info: "I built this portfolio to house all my projects. Made with React and Tailwind CSS.",
+    tech: ["React", "Tailwind CSS"],
+    sourceCode: "https://github.com/hjaox/hjaox.github.io/tree/react",
+    appLink: "https://hectorobanana.netlify.app/",
+    preview: "./media/projects/personalPortfoliov1.1-preview.png",
+    shortVid: "",
+  },
+};
